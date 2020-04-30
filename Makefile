d0f5237c36f6159e9db9668372b4b20d13aa4492
Barna Mink <barnamink@gmail.com>
2020-04-30 01:19:00 +0100

    .

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-P = redissms
+P1 = redissms_sender
+P2 = redissms_receiver
 OBJS = main.o hiredis_helper.o
 CFLAGS = -g -Wall -Wstrict-prototypes
 LDLIBS = -lb -lhiredis
 
-$(P): $(OBJS)
-	$(CC) -o $(P) $(LDFLAGS) $(OBJS) $(LDLIBS)
+all: $(P1) $(P2)
+
+$(P1): $(OBJS)
+	$(CC) -o $(P1) $(LDFLAGS) $(OBJS) $(LDLIBS)
+
+$(P2): $(OBJS)
+	$(CC) -o $(P2) $(LDFLAGS) $(OBJS) $(LDLIBS)
 
 clean:
-	rm -f *o; rm -f $(P)
+	rm -f *o; rm -f $(P1); rm -f $(P2)
--- main.c
+++ main.c
@@ -1,9 +1,62 @@
 #include <stdio.h>
+#include <libgen.h>
+#include "bstr.h"
+#include "blog.h"
+
+
+
+#define EXECN_SENDER	"redissms_sender"
+#define EXECN_RECEIVER	"redissms_receiver"
+
+void sender_loop(void);
+
 
 int
 main(int argc, char **argv)
 {
-	printf("hello, world\n");
+	char	*execn;
+	int	ret;
+
+	execn = basename(argv[0]);
+	if(xstrempty(execn)) {
+		fprintf(stderr, "Can't get executable name\n");
+		goto end_label;
+	}
+
+	ret = blog_init(execn);
+	if(ret != 0) {
+		fprintf(stderr, "Could not initialize logging: %s\n",
+		    strerror(ret));
+		goto end_label;
+	}
+
+	if(!xstrcmp(execn, EXECN_SENDER)) {
+		if(argc != 1) {
+			fprintf(stderr, "No arguments should be speficied\n");
+			goto end_label;
+		}
+
+		sender_loop();
+
+	} else
+	if(!xstrcmp(execn, EXECN_RECEIVER)) {
+	} else {
+		fprintf(stderr, "Do not recognize executable name.\n");
+		goto end_label;
+	}
+
+
+end_label:
+
+	blog_uninit();
+
 	return 0;
+}
+
+
+
+void
+sender_loop(void)
+{
 }
 
